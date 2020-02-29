e9c86492a7c1ec7594b31558f3198c3bc58e6e58
Simon Lagos <simon.lagos.sallhed@gmail.com>
2020-02-29 14:08:52 +0900

    Replace Parcel with Rollup

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+export default [{
+  input: 'index.js',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+    },
+    // {
+    //   file: 'dist/index.js',
+    //   format: 'esm',
+    // },
+  ],
+}]
