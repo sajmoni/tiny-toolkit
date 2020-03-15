e4d5c571ddf709902272b469ec0e1ff60d9e38a2
Simon Lagos <simon.lagos.sallhed@gmail.com>
2020-03-15 12:48:28 +0900

    Maintenance

---
+++ build-test.sh
@@ -0,0 +1,14 @@
+set -e
+
+runCommand() {
+  echo "=== $1 ==="
+  $1
+}
+
+runCommand "yarn clean"
+runCommand "yarn build"
+runCommand "yarn pack --filename tiny-toolkit.tgz"
+runCommand "cd example"
+runCommand "yarn refresh"
+runCommand "yarn test"
+runCommand "cd -"
